894fd2117ebf61bbc2849df1a27a832e278f5b07
jeevan-p-gowda <jeevanputtaswamy@gmail.com>
2024-04-07 17:37:40 +0530

    config

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,28 @@ export default defineConfig({
     ],
     projects: [
         {
+            name: "setup",
+            testDir: "./globals",
+            testMatch: "setup.ts",
+            teardown: "teardown",
+        },
+        {
+            name: "teardown",
+            testDir: "./globals",
+            testMatch: "teardown.ts",
+        },
+        {
             name: "chromium",
             use: { ...devices["Desktop Chrome"], headless: false, channel: "chrome", testIdAttribute: "data-test-id" }, // Executing in the Chrome channel
+            dependencies: ["setup"], // Important for chromium project to use setup process as dependency before initiating the tests in chromium suite
         },
+        {
+            name: "mobile_emulation",
+            use: {
+                ...devices["iPhone 12 Mini"],
+                headless: false
+            },
+            testMatch: ["**/*.me.spec.ts"]
+        }
     ],
 });
